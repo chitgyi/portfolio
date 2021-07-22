15f4024d5b1b0459774779b08df3d17a2bba7a66
Chit Ye Aung <chityeaung.mti@gmail.com>
2021-07-22 18:58:36 +0630

    uncomment home content

--- src/layout/home.content.js
+++ src/layout/home.content.js
@@ -3,15 +3,14 @@ import React from 'react'
 export const HomeContent = () => {
     return (
         <div>
-            {/* <p class="text-3xl font-bold text-gray-200 text-opacity-50 flex">Hi,</p>
+            <p class="text-3xl font-bold text-gray-200 text-opacity-50 flex">Hi,</p>
             <div className="flex">
                 <p class="text-3xl font-bold text-gray-200 text-opacity-50">I’m</p>
                 <p class="text-3xl font-bold text-gray-200">&nbsp;CHIT YE AUNG,</p>
             </div>
             <div className="font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-b from-green-300 to-yellow-300">
                 Mobile App Developer &amp; Full Stack Developer.
-            </div> */}
-            <p>Home Page</p>
+            </div>
         </div>
     )
 }
